644f98c844a34869a51382dd0d78061ca16ee901
ashishthomas22298 <athomas22298@gmail.com>
2021-07-21 18:58:22 -0400

    product update: sku, name,ribbon,categoryId verification complete

--- controllers/product.js
+++ controllers/product.js
@@ -55,7 +55,6 @@ exports.create = function(req, res) {
             }
 
             let jsonData = JSON.parse(fields.jsonData);
-            console.log(jsonData);
 
             //***************** Parsing Fields *************************
 
@@ -258,20 +257,22 @@ exports.create = function(req, res) {
             }
             //Assigning the images to the product object
             product.images = images;
+            /************* images validation ends *************/
+
+
 
             product.save((err, data) => {
-                    if (err) {
-                        removeErrorFiles(files.images, productDir);
-                        return res.status(400).json({
-                            "errors": errorHandler(err)
-                        });
-                    }
-                    return res.json({
-                        data,
-                        msg: 'Product successfully created'
+                if (err) {
+                    removeErrorFiles(files.images, productDir);
+                    return res.status(400).json({
+                        "errors": errorHandler(err)
                     });
-                })
-                /************* images validation ends *************/
+                }
+                return res.json({
+                    data,
+                    msg: 'Product successfully created'
+                });
+            })
 
         } catch (err) {
             return handleError(res, err);
@@ -285,6 +286,292 @@ exports.read = function(req, res) {
     return res.json(req.product);
 }
 
+
+
+exports.update = function(req, res) {
+
+    //Initializing the formidable
+    let form = new formidable.IncomingForm();
+    form.encoding = 'utf-8';
+    form.keepExtensions = true;
+    //accept multiple files
+    form.multiples = true;
+    // directory to upload files
+    form.uploadDir = tempFolderPath;
+
+
+    // parsing the form data
+    form.parse(req, function(err, fields, files) {
+        try {
+
+            // if there is any error occurs in parsing form
+            if (err) {
+                throw JSON.stringify({
+                    message: 'Image could not be uploaded',
+                    param: 'images',
+                    files: files,
+                    fileTransfer: 'false'
+                });
+            }
+
+            let jsonData = JSON.parse(fields.jsonData);
+            console.log(jsonData);
+
+            //***************** Parsing Fields *************************
+
+            let product = req.product;
+            let { sku, name, ribbon, categoryId, onePrice, costPrice, margin, stickerPrice, onSale, discount, description, additionalInfo, productOptions } = jsonData;
+
+
+            /******** sku validation ********/
+            if (!(sku === product.sku)) {
+                check('sku', { sku, files });
+                try {
+                    fs.renameSync(path.join(productDirectory, _.kebabCase(product.sku)), path.join(productDirectory, _.kebabCase(sku)));
+                } catch (err) {
+                    console.log(err);
+                    if (err.code === 'EEXIST')
+                        throw JSON.stringify({
+                            message: 'sku already exist',
+                            param: 'sku',
+                            files: files,
+                        });
+                    else if (err.code === 'ENOENT')
+                        throw JSON.stringify({
+                            message: 'Invalid image path',
+                            param: 'Image Directory',
+                            productDir: productDir,
+                            files: files
+                        });
+                    else {
+                        throw err;
+                    }
+                }
+
+                //Assigning the sku value to the product object
+                product.sku = sku;
+            }
+            /******** sku validation ends ********/
+
+
+
+            /************* name validation *************/
+            if (!(name === product.name)) {
+                check('name', { name, files });
+                //Assigning the name to the product object
+                product.name = name;
+            }
+            /************* name validation ends *************/
+
+
+
+            /************* ribbon validation *************/
+            if (ribbon) {
+                if (!(ribbon === product.ribbon)) {
+                    check('ribbon', { ribbon, files });
+                    //Assigning the ribbon to the product object
+                    product.ribbon = ribbon;
+                }
+            } else {
+                if (product.ribbon) {
+                    product.ribbon = undefined;
+                }
+            }
+            /************* ribbon validation ends *************/
+
+
+
+            /************* categoryId validation *************/
+            if (!(categoryId === product.categoryId)) {
+                check('categoryId', { categoryId, files });
+                //Assigning the categoryId to the product object
+                product.categoryId = categoryId;
+            }
+            /************* categoryId validation ends *************/
+
+
+
+            // /************* onePrice validation *************/
+            // check('onePrice', { onePrice, files });
+            // //Assigning the onePrice to the product object
+            // product.onePrice = onePrice;
+            // /************* onePrice validation ends *************/
+
+
+
+            // /************* costPrice validation *************/
+            // if (onePrice) {
+            //     check('costPrice', { costPrice, files });
+            //     //Assigning the costPrice to the product object
+            //     product.costPrice = costPrice;
+            // }
+            // /************* costPrice validation ends *************/
+
+
+
+            // /************* stickerPrice validation *************/
+            // if (onePrice) {
+            //     check('stickerPrice', { stickerPrice, costPrice, files });
+            //     //Assigning the stickerPrice to the product object
+            //     product.stickerPrice = stickerPrice;
+            // }
+            // /************* stickerPrice validation ends *************/
+
+
+
+            // /************* margin validation *************/
+            // if (onePrice) {
+            //     check('margin', { margin, costPrice, stickerPrice, files });
+            //     //Assigning the margin to the product object
+            //     product.margin = margin;
+            // }
+            // /************* margin validation ends *************/
+
+
+
+            // /************* onSale validation *************/
+            // check('onSale', { onSale, files });
+            // //Assigning the onSale to the product object
+            // product.onSale = onSale;
+            // /************* onSale validation ends *************/
+
+
+
+            // /************* discount validation *************/
+            // if (onSale) {
+            //     check('discount', { discount, files });
+            //     //Assigning the discount to the product object
+            //     product.discount = discount;
+            // }
+            // /************* discount validation ends *************/
+
+
+
+            // /************* description validation *************/
+            // check('description', { description, files });
+            // //Assigning the description to the product object
+            // product.description = description;
+            // /************* description validation ends *************/
+
+
+
+            // /************* additionalInfo validation *************/
+            // if (additionalInfo) {
+            //     check('additionalInfo', { additionalInfo, files });
+            //     //Assigning the additionalInfo to the product object
+            //     product.additionalInfo = additionalInfo;
+            // }
+            // /************* additionalInfo validation ends *************/
+
+
+
+            // /************* productOptions validation *************/
+            // if (productOptions) {
+            //     check('productOptions', { productOptions, onePrice, costPrice, stickerPrice, margin, files });
+            //     //Assigning the productOptions to the product object
+            //     product.productOptions = productOptions;
+            // }
+            // /************* productOptions validation ends *************/
+
+
+
+            // /************* images validation *************/
+            // // Folder to save all the images of the product 
+            // const folderName = _.kebabCase(sku);
+            // let productDir = path.join(productDirectory, folderName);
+
+            // // array to store images info
+            // let images = [];
+
+            // // checking if the image is present in the form
+            // if (files.images && files.images != '') {
+            //     try {
+            //         let imageList = [];
+            //         //Creating the directory to store the images
+            //         fs.mkdirSync(productDir);
+
+            //         //Loop to go through each image
+            //         for (let image of files.images) {
+            //             // temporary location of the image
+            //             let oldPath = image.path;
+            //             // index of the dot before the image extension
+            //             let indexOfDot = image.name.indexOf('.');
+            //             // Name of the image
+            //             let name = _.kebabCase(image.name.substring(0, indexOfDot));
+            //             // extension of the image
+            //             let extension = image.name.substring(indexOfDot);
+            //             // New location of the image
+            //             let newPath = path.join(tempFolderPath, '../', folderName, name + extension);
+
+            //             // checking for the file extension
+            //             if (!(extension.toLowerCase() === '.jpg' || extension.toLowerCase() === '.jpeg' || extension.toLowerCase() === '.bmp' || extension.toLowerCase() === '.png' || extension.toLowerCase() === '.gif' || extension.toLowerCase() === '.tiff' || extension.toLowerCase() === '.svg' || extension.toLowerCase() === '.webp')) {
+            //                 throw JSON.stringify({
+            //                     message: 'Invalid image format',
+            //                     param: 'images',
+            //                     productDir: productDir,
+            //                     files: files
+            //                 });
+            //             }
+            //             // adding images info in an array
+            //             imageList.push({
+            //                 oldPath,
+            //                 name,
+            //                 extension,
+            //                 folderName,
+            //                 newPath,
+            //             });
+            //         }
+            //         // loop to go through each image one by one
+            //         for (let image of imageList) {
+            //             //moving image from temp folder to the product folder
+            //             fs.renameSync(image.oldPath, image.newPath);
+            //             images.push({
+            //                 name: image.name,
+            //                 extension: image.extension,
+            //                 path: image.newPath
+            //             });
+            //         }
+            //     } catch (err) {
+            //         if (err.code === 'EEXIST')
+            //             throw JSON.stringify({
+            //                 message: 'sku already exist',
+            //                 param: 'sku',
+            //                 files: files,
+            //             });
+            //         else if (err.code === 'ENOENT')
+            //             throw JSON.stringify({
+            //                 message: 'Invalid image path',
+            //                 param: 'Image Directory',
+            //                 productDir: productDir,
+            //                 files: files
+            //             });
+            //         else {
+            //             throw err;
+            //         }
+            //     }
+            // }
+            // //Assigning the images to the product object
+            // product.images = images;
+            // removeErrorFiles(files.images);
+            product.save((err, data) => {
+                    if (err) {
+                        return res.status(400).json({
+                            "errors": errorHandler(err)
+                        });
+                    }
+                    return res.json({
+                        data,
+                        msg: 'Product successfully created'
+                    });
+                })
+                /************* images validation ends *************/
+
+            // res.json({ msg: 'successful' })
+        } catch (err) {
+            return handleError(res, err);
+        }
+    });
+}
 
 
 exports.remove = function(req, res) {
@@ -320,7 +607,7 @@ function handleError(res, err) {
     let customError = JSON.parse(err);
 
     // files present
-    if (customError.files) {
+    if (customError.files && customError.files.images) {
 
         // product directory is present
         if (customError.productDir) {
--- routes/product.js
+++ routes/product.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const { requireSignin, isAuth, isEmployee, isAdmin } = require('../controllers/auth');
-const { read, productById, create, remove } = require('../controllers/product');
+const { productById, create, read, update, remove } = require('../controllers/product');
 const { userById } = require('../controllers/user');
 const level = 1;
 
 router.post('/product/create/:userId', requireSignin, isAuth, isEmployee(level), create);
 router.get('/product/:productId', read);
+router.put('/product/edit/:productId/:userId', requireSignin, isAuth, isEmployee(level), update);
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isEmployee(level), remove);
 
 router.param('userId', userById);
